1bb44defff308d00f06e4a852c327c37885ce03b
Moreno <jesusmorenomm1@gmail.com>
2026-02-23 21:13:06 -0600

    Http server require fixed

--- Part-2/1.http.js
+++ Part-2/1.http.js
@@ -1,9 +1,9 @@
 const http = require('node:http')
-const { findAvailablePort } = require('./10.free-port')
+const { findAvailablePort } = require('../Part-1/10.free-port.js')
 
-console.log(process.env)
+// console.log(process.env)
 
-const desiredPort = process.env.PORT ?? 3000
+const desiredPort = process.env.PORT ?? 1234
 
 const server = http.createServer((req, res) => {
   console.log('request received')
