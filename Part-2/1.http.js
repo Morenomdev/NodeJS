3f9134713357f0003f61909bf4f6ac67950057b4
Moreno <jesusmorenomm1@gmail.com>
2026-02-26 20:43:32 -0600

    Created an API with pokeAPI JSON format and switch

--- Part-2/1.http.js
+++ Part-2/1.http.js
@@ -1,21 +1,42 @@
 const http = require('node:http')
-const { findAvailablePort } = require('../Part-1/10.free-port.js')
+const fs = require('node:fs')
+// const { findAvailablePort } = require('../Part-1/10.free-port.js')
 
 // console.log(process.env)
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
-  console.log('request received')
-  res.end('Hello world')
-})
+const processRequest = (req, res) => {
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end('Welcome to my pagé')
+  } else if (req.url === '/node.webp') {
+    res.setHeader('Content-Type', 'image/webp')
 
-findAvailablePort(desiredPort).then((port) => {
-  server.listen(port, () => {
-    console.log(`server listening http://localhost:${server.address().port}`)
-  })
-})
+    fs.readFile('./node.webp', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('500 Internal Server Error')
+      } else {
+        res.setHeader('Content-Type', 'image/webp')
+        res.end(data)
+      }
+    })
+  } else if (req.url === '/contact') {
+    res.end('Contact')
+  } else {
+    res.statusCode = 404 // not found
+    res.end('404')
+  }
+}
 
-// server.listen(0, () => {
-//   console.log(`server listening http://localhost:${server.address().port}`)
+const server = http.createServer(processRequest)
+
+server.listen(desiredPort, () => {
+  console.log(`server listening http://localhost:${desiredPort}`)
+})
+// findAvailablePort(desiredPort).then((port) => {
+//   server.listen(port, () => {
+//     console.log(`server listening http://localhost:${server.address().port}`)
+//   })
 // })
---
+++ Part-2/2.routing.js
@@ -0,0 +1,33 @@
+const http = require('node:http')
+
+// commonJS -> modulos clasicos de node
+const dittoJSON = require('./pokemon/ditto.json')
+
+const processRequest = (req, res) => {
+  const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('404')
+      }
+
+    case 'POST':
+      switch (url) {
+        case '/pokemon':
+          // let body = ''
+      }
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+  console.log(`Server runing at localhost:${server.address().port}`)
+})
