a4d59e295e8abc328ea7fe9c63fdd1f11e48ab2e
Moreno <jesusmorenomm1@gmail.com>
2026-03-01 21:06:13 -0600

    Added filter by genre and find movie by id

--- Part-3/app.js
+++ Part-3/app.js
@@ -15,7 +15,24 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
   res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find((movie) => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
 })
 
 app.listen(PORT, () => {
